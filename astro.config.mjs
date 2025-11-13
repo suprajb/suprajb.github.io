9c702731a9a0eede2c9727b1efe0e929db51793e
Supraj Bachawala <bachawalasupraj@gmail.com>
2025-11-13 03:39:24 -0500

    remove dead links

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   // add yur domain name here
-  site: 'https://yourdomain.com',
+  site: 'https://suprajb.github.io',
   compressHTML: true,
   integrations: [sitemap()]
 });
